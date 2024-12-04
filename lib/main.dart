f85d99efed584c91cd8430a96722d8dd07ce452c
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-04 16:53:03 +0900

    welcome_page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_market_app/ui/pages/welcome/welcome_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
+  // flutter pub add flutter_riverpod 설치하고나서 꼭 이거 입력해야함.
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +13,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        highlightColor: Colors.orange,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor:
+                WidgetStatePropertyAll(Colors.white), // 버튼의 글자나 아이콘 색깔
+            backgroundColor: WidgetStatePropertyAll(Colors.orange), // 버튼의 바탕색
+            minimumSize: WidgetStatePropertyAll(
+                Size.fromHeight(50)), // 가로는 최대치로 늘어나고 세로는 설정해준 값만큼만.
+            // ElevatedButton의 크기를 늘리려면 컨테이너 사이즈드 박스 위젯으로 감싸줘야 한다!
+            textStyle: WidgetStatePropertyAll(
+              TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                // 여기까지 입력하고 시뮬레이터 화면 에러뜨는거 같아도 커맨드 시프트 F5 누르면 에러 사라짐.
+              ),
+            ),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                    8), // 버튼 모서리 설정 (여기까지 해주고 패팅은 welcome페이지에서 설정해줌)
+              ),
+            ),
+          ),
+        ),
+      ),
       home: WelcomePage(),
     );
   }
--- lib/ui/pages/welcome/welcome_page.dart
+++ lib/ui/pages/welcome/welcome_page.dart
@@ -1,11 +1,90 @@
+/* Column
+   - Image
+   - Text
+   - Text
+   - ElevatedButton
+   - Text
+*/
+
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/address_search/address_search_page.dart';
+import 'package:flutter_market_app/ui/pages/home/home_page.dart';
+import 'package:flutter_market_app/ui/pages/login/login_page.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('WelcomePage'),
+      // appBar: AppBar(), 여기서 앱바는 지워줬다.
+      body: SafeArea(
+        child: SizedBox.expand(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Spacer(),
+                Image.asset(
+                  'assets/welcome.png',
+                  height: 250,
+                ),
+                Text(
+                  '당신 근처의 마켓',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '동네라서 가능한 모든 것\n지금 내 동네를 선택하고 시작해 보세요.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return AddressSearchPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('시작하기'),
+                ), // ElevatedButton의 스타일을 만들어줘야하는데
+                // 앱 전체적으로 버튼 스타일이 통일되어야 하니까 여기서 말고 main.dart로 가서
+                // MaterialApp에 theme속성을 이용해서 전체 앱내에서 ElevatedButton 전체 테마를 설정해줘야 한다.
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 50, // 항상 버튼성의 위젯을 사용할때는 최소 터치반경 크기를 44 이상으로 해줄 것!
+                    color: Colors.transparent,
+                    alignment: Alignment.center,
+                    child: Text(
+                      //
+                      '이미 계정이 있나요? 로그인', // 이 부분도 버튼처럼 쓰이니까 Container로 만들어서 바꿔줌.
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
