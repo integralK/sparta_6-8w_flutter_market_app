0121fcb7f9da8142862dd52112470ee4f0d52d75
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-09 17:05:58 +0900

    chat_tab

--- lib/ui/pages/home/_tab/chat_tab/widgets/chat_tab_list_view.dart
+++ lib/ui/pages/home/_tab/chat_tab/widgets/chat_tab_list_view.dart
@@ -1,31 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/chat_detail/chat_detail_page.dart';
 import 'package:flutter_market_app/ui/widgets/user_profile_image.dart';
 
 class ChatTabListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView(
-        children: [
-          item(),
-          item(),
-          item(),
-        ],
+      child: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return item();
+        },
+        separatorBuilder: (context, index) {
+          return Divider(height: 1);
+        },
       ),
     );
   }
 
   Widget item() {
-    return Container(
-      height: 80,
-      child: Row(
-        children: [
-          UserProfileImage(
-            dimension: 50,
-            imgUrl: 'https://picsum.photos/200/300',
+    return Builder(builder: (context) {
+      return GestureDetector(
+        // wrap with builder
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return ChatDetailPage();
+              },
+            ),
+          );
+        },
+        child: Container(
+          height: 80,
+          color: Colors.transparent,
+          child: Row(
+            children: [
+              UserProfileImage(
+                dimension: 50,
+                imgUrl: 'https://picsum.photos/200/300',
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center, // 세로 가운데 정렬
+                  crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 배치
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '오상구님',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          '1분전',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text('네고 가능한가요?'),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ),
+      );
+    });
   }
 }
