e294c3ab629bfa1d1b2ff6f00e42033726e18595
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-09 20:24:46 +0900

    analysis_options.yaml

--- lib/ui/pages/home/_tab/my_tab/widgets/my_profile_box.dart
+++ lib/ui/pages/home/_tab/my_tab/widgets/my_profile_box.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_market_app/ui/widgets/user_profile_image.dart';
 
 class MyProfileBox extends StatelessWidget {
+  const MyProfileBox({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -10,14 +12,26 @@ class MyProfileBox extends StatelessWidget {
           dimension: 50,
           imgUrl: 'https://picsum.photos/200/300',
         ),
+        SizedBox(width: 10),
         Expanded(
-            child: Text(
-          '오상구',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+          child: Text(
+            '오상구',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ))
+        ),
+        Container(
+          child: Text(
+            '프로필 수정',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ],
     );
   }
