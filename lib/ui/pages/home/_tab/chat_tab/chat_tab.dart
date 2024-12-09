697e1fb11d56a34c23e478315c0c595ebc76befd
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-09 15:35:16 +0900

    SizedBox.square(dimension)

--- lib/ui/pages/home/_tab/chat_tab/chat_tab.dart
+++ lib/ui/pages/home/_tab/chat_tab/chat_tab.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/home/_tab/chat_tab/widgets/chat_tab_app_bar.dart';
+import 'package:flutter_market_app/ui/pages/home/_tab/chat_tab/widgets/chat_tab_list_view.dart';
 
 class ChatTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ChatTab'),
+    return SizedBox.expand(
+      // chat tab의 크기를 무한히 확장해준다.
+      child: Column(
+        children: [
+          ChatTabAppBar(),
+          ChatTabListView(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/ui/pages/home/_tab/chat_tab/widgets/chat_tab_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChatTabListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: [
+          item(),
+          item(),
+          item(),
+        ],
+      ),
+    );
+  }
+
+  Widget item() {
+    return Container(
+      height: 80,
+      child: Row(
+        children: [
+          SizedBox.square(
+            dimension: 50,
+            /* SizedBox(
+               width: 50, height: 50,
+               가로 세로 같을때 sizedbox를 그냥 넣는게 아니라 뒤에 .square 생성자를 넣고 
+               demension이라는 속성을 넣으면 위에와 같이 가로세로 크기 입력가능 */
+            child: ClipRRect(
+              // 이것만 해서는 원모양이 잘 안나올수 있고, 위에 sizedbox 만들어서 크기 넣어줘야 함.
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(
+                'https://picsum.photos/200/300',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
