f5f7faa14c4ea015194025b471b40f3cecb2c783
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-10 20:40:08 +0900

    ProductCategoryBox onSelected

--- lib/ui/pages/product_write/widgets/product_category_box.dart
+++ lib/ui/pages/product_write/widgets/product_category_box.dart
@@ -3,6 +3,52 @@ import 'package:flutter/material.dart';
 class ProductCategoryBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('ProductCategoryBox');
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: PopupMenuButton<String>(
+        position: PopupMenuPosition.under,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        onSelected: (value) {
+          print(value);
+        },
+        itemBuilder: (context) {
+          return [
+            categoryItem('디지털 가전', true),
+            categoryItem('생활용품', false),
+          ];
+        },
+        // onSelected; ,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.all(10), // 버튼 크기 좀 늘려줬음
+          child: Text(
+            '카테고리 선택',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  PopupMenuItem<String> categoryItem(String text, bool isSelected) {
+    return PopupMenuItem<String>(
+      value: text,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: isSelected ? FontWeight.bold : null,
+          color: isSelected ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
   }
 }
