92690fd884c013a4798c481c5f3bf0a655078958
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-10 12:52:41 +0900

    ProductDetailPicture PageView.builder

--- lib/ui/pages/product_detail/widgets/product_detail_picture.dart
+++ lib/ui/pages/product_detail/widgets/product_detail_picture.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class ProductDetailPicture extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('ProductDetailPicture');
+    return SizedBox(
+      // PageView를 감싸는 부모위젯의 크기가 항상 있어야 한다!!!
+      height: 500,
+      child: PageView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Image.network(
+            'https://picsum.photos/200/300',
+            fit: BoxFit.cover,
+          );
+        },
+      ),
+    );
   }
 }
