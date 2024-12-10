86b427a0c6c3c7396b8b9701d54e394ca93b086b
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-10 14:14:44 +0900

    TextEditingController dispose

--- lib/ui/pages/product_write/product_write_page.dart
+++ lib/ui/pages/product_write/product_write_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ProductWritePage extends StatelessWidget {
+class ProductWritePage extends StatefulWidget {
+  @override
+  State<ProductWritePage> createState() => _ProductWritePageState();
+}
+
+class _ProductWritePageState extends State<ProductWritePage> {
+  final titleController = TextEditingController();
+  final priceController = TextEditingController();
+  final contentController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    priceController.dispose();
+    contentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text('ProductWrite'),
+      body: ListView(
+        children: [
+          //
+        ],
+      ),
     );
   }
 }
---
+++ lib/ui/pages/product_write/widgets/product_category_box.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProductCategoryBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('ProductCategoryBox');
+  }
+}
