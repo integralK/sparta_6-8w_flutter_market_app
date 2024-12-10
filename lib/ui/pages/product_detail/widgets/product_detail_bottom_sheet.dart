088a29b56ab53fa52776440a4171a6ba3f60f2c3
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-10 12:41:47 +0900

    ProductDetailBottomSheet

--- lib/ui/pages/product_detail/widgets/product_detail_bottom_sheet.dart
+++ lib/ui/pages/product_detail/widgets/product_detail_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/chat_detail/chat_detail_page.dart';
 
 class ProductDetailBottomSheet extends StatelessWidget {
   ProductDetailBottomSheet(this.bottomPadding);
@@ -17,7 +18,57 @@ class ProductDetailBottomSheet extends StatelessWidget {
           Expanded(
             child: Row(
               children: [
-                Icon(CupertinoIcons.heart),
+                GestureDetector(
+                  onTap: () {
+                    print('하트 터치됨');
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.transparent,
+                    child: Icon(CupertinoIcons.heart),
+                  ),
+                ),
+                VerticalDivider(
+                  width: 20,
+                  indent: 10, // 세로 디바이더의 높이 윗부분 조절
+                  endIndent: 10, // 세로 디바이더의 높이 아랫부분 조절 (패딩개념?)
+                  color: Colors.grey,
+                ),
+                Expanded(
+                  child: Text(
+                    '100,000원',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  // ElevatedButton의 사이즈를 조절해서 만들기위해 이렇게 하는거!
+                  width: 100,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ChatDetailPage();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      '채팅하기',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20), // 버튼 우측에 여백 좀 만들려고
               ],
             ),
           ),
